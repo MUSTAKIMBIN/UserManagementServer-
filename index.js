041d48ce5ef88680c0010c47221a874b1aeae42c
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-10-29 15:31:54 +0600

    resived data from clint

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("First Server Deployment Successful");
@@ -8,14 +12,19 @@ app.get("/", (req, res) => {
 
 const user = [
   { id: 1, name: "mustakim", email: "mustakim@game.com" },
-  { id: 1, name: "vaiya", email: "vaiya@game.com" },
-  { id: 1, name: "AlexHOr", email: "AlexHOr@game.com" },
-  { id: 1, name: "LilaHOr", email: "LilaHOr@game.com" },
+  { id: 2, name: "vaiya", email: "vaiya@game.com" },
+  { id: 3, name: "AlexHOr", email: "AlexHOr@game.com" },
+  { id: 4, name: "LilaHOr", email: "LilaHOr@game.com" },
 ];
 
 // send all user to client
 app.get("/users", (req, res) => {
   res.send(user);
+});
+
+// post user to server
+app.post("/users", (req, res) => {
+  console.log(`post mathod called`, req.body);
 });
 
 app.listen(port, () => {
